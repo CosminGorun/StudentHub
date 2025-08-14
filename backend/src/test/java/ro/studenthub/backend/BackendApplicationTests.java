f86279c524ec5a749b2f5de2fcd38526e2c9a95f
CosminGorun <gorun.cosmin003@gmail.com>
2025-08-14 13:34:52 +0300

    adaugare backend

---
+++ backend/src/test/java/ro/studenthub/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.studenthub.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
